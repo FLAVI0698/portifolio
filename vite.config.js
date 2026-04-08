bf689fa2ab43b929937367e514a8a756c3cef7f5
FLAVI0698 <flaviogouveia1998@gmail.com>
2026-04-07 23:00:46 -0300

    update gh pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-   base: '/portifolio/',
-  plugins: [react(),  tailwindcss()],
+  base: '/portifolio/',
+  plugins: [react(), tailwindcss()],
 })
